980c3a460bc59076a40a4e4d6e6e69d103cecbd1
houssam005 <houssam005@hotmail.com>
2022-11-10 17:34:24 +0100

    fd

---
+++ simple program/client.py
@@ -0,0 +1,18 @@
+from vidstream import AudioSender
+from vidstream import AudioReceiver
+import threading 
+import socket
+#"64.44.97.254"
+ip = socket.gethostbyname(socket.gethostname())
+
+receiver = AudioReceiver(ip,9999)
+receive_thread = threading.Thread(target = receiver.start_server)
+
+sender = AudioSender("64.44.97.254",5554)
+sender_thread = threading.Thread(target = sender.start_stream)
+
+receive_thread.start()
+sender_thread.start()
+
+
+
