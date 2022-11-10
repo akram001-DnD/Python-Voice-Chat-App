9bf67894015ba980822b21e67957e958ac7be021
houssam005 <houssam005@hotmail.com>
2022-11-10 19:58:47 +0100

    vv

--- simple program/server.py
+++ simple program/server.py
@@ -1,9 +1,77 @@
-from vidstream import StreamingServer
+import socket
+import threading
 
-server = StreamingServer('0.0.0.0', 9999)
-server.start_server()
 
-# Other Code
+class LaunchServer:
 
-# When You Are Done
-server.stop_server()
+    def __init__(self, host, port, slots=8,frame_chunk=4096):
+        self.__host = host
+        self.__port = port
+
+        self.__slots = slots
+        self.__used_slots = 0
+
+        self.__frame_chunk = frame_chunk
+
+
+        self.__server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__server_socket.bind((self.__host, self.__port))
+
+        self.__block = threading.Lock()
+        self.__running = False
+
+        self.users = []
+
+    def start_server(self):
+        if self.__running:
+            print("Audio server is running already")
+        else:
+            self.__running = True
+            thread = threading.Thread(target=self.__server_listening)
+            thread.start()
+
+    def __server_listening(self):
+        self.__server_socket.listen()
+        while self.__running:
+            self.__block.acquire()
+            connection, address = self.__server_socket.accept()
+            self.users.append((connection, address))
+            print(f"Client {connection} Have Been Connected!")
+            if self.__used_slots >= self.__slots:
+                print("Connection refused! No free slots!")
+                connection.close()
+                self.__block.release()
+                continue
+            else:
+                self.__used_slots += 1
+
+            self.__block.release()
+            thread = threading.Thread(target=self.__client_connection, args=(connection, address,))
+            thread.start()
+
+    def __client_connection(self, connection, address):
+        while self.__running:
+            data = connection.recv(self.__frame_chunk)
+            for client in self.users:
+                try:
+                    if client[0] != connection:
+                        self.__server_socket.send(data)
+                except:
+                    print(f"Client {client[1]} Disconnected!")
+                    client[0].close()
+                    self.users.remove(client)
+
+
+    def stop_server(self):
+        if self.__running:
+            self.__running = False
+            closing_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            closing_connection.connect((self.__host, self.__port))
+            closing_connection.close()
+            self.__block.acquire()
+            self.__server_socket.close()
+            self.__block.release()
+        else:
+            print("Server not running!")
+
+        
