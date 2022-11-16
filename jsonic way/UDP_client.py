7ecd9945add37a20e31a90a6b79d156b2237b985
DnD001 <akram01boudj@gmail.com>
2022-11-16 12:37:04 +0100

    added  two files with a json type communication

---
+++ jsonic way/UDP_client.py
@@ -0,0 +1,138 @@
+# Welcome to PyShine
+# This is client code to receive video and audio frames over UDP
+
+import socket
+import threading,  pyaudio, time, queue
+import sys
+import numpy as np
+import pickle
+########################
+#Jotar
+
+####### wait for unreal client to give us the server info
+#PlayerInfo = {"PlayerId":PlayerId,"type":"voiceMessage", ,"HostAddress","64.44.97.254", "TargetIds":[1,2,7],"Volume":[0.5,0.2,1.0] }
+#send PlayerInfo to the server
+
+######################
+
+host_name = socket.gethostname()
+# host_ip="64.44.97.254"
+UDP_port = 9633
+TCP_port = 9632
+ID = "Drogba"
+# For details visit: www.pyshine.com  
+target_IDs = [["Roberto", 0.7]]
+PlayerInfo = {"ID":ID, "type": "voiceMsg","HostAddress":"64.44.97.254","TargetIDs":target_IDs }
+host_ip = PlayerInfo.HostAddress
+
+
+def audio_stream_UDP():
+    BUFF_SIZE = 65536
+    #! Initializing UDP Connection
+    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+    print(f"Connected to server {host_ip}:{UDP_port} ")
+
+    #! Initializing TCP Connection
+    try:
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as msg:
+        print("Socket creation error: ", str(msg))
+    try:
+        tcp.connect((host_ip,TCP_port))
+    except ConnectionRefusedError:
+        print("This Server Is Dead!, Try Again Later.")
+        sys.exit()
+    except OSError:
+        print("Unknown Error Happened!, Connection Closed.")
+        sys.exit()
+    tcp.send(ID.encode())
+
+    p = pyaudio.PyAudio()
+    CHUNK = 10*1024
+    hear = p.open(format=p.get_format_from_width(2),
+					channels=2,
+					rate=44100,
+					output=True,
+					frames_per_buffer=CHUNK)
+                    
+    record = p.open(format=p.get_format_from_width(2),
+					channels=2,
+					rate=44100,
+					input=True,
+					frames_per_buffer=CHUNK)			
+
+
+    # answer,_ = udp.recvfrom(BUFF_SIZE)
+    # print(answer.decode())
+    q = queue.Queue(maxsize=10000)
+
+
+    def set_volume(data, volume):
+        chunk = np.frombuffer(data, dtype = np.int16)
+        chunk = chunk * volume
+        data = chunk.astype(np.int16)
+        data = data.tobytes()      # Activate this for recieved audio
+        return data
+
+
+    def getAudioData():
+        while True:
+            try:
+                frame,_= s.recvfrom(BUFF_SIZE)
+                q.put(frame)
+                # print('[Queue size while loading]...',q.qsize())
+            except:
+                s.close()
+                print('Audio closed')
+                sys.exit()
+
+    def sendAudioData():
+        while True:
+            if PlayerInfo.type == "voiceMsg":
+                voice = record.read(CHUNK)
+                data = [PlayerInfo, voice]
+                data=pickle.dumps(data)
+                try:
+                    s.sendto(data,(host_ip, UDP_port))
+                except OSError:
+                    sys.exit()
+    
+
+    # def send_conn_check_msg():
+    #     while True:
+    #         data = s.recvfrom(BUFF_SIZE)
+    #         try:
+    #             if data.decode() == "Still Connected?":
+    #                 s.sendto("Still Connected".encode(),(host_ip, port))
+    #         except UnicodeDecodeError:
+    #             pass
+    
+    s.sendto(ID.encode(),(host_ip, UDP_port))
+    msg, _ = s.recvfrom(BUFF_SIZE)
+    print(msg.decode())
+    t1 = threading.Thread(target=getAudioData, args=())
+    t1.start()
+    t2 = threading.Thread(target=sendAudioData, args=())
+    t2.start()
+    # t3 = threading.Thread(target=send_conn_check_msg, args=())
+    # t3.start()
+
+    # time.sleep(5)
+
+
+    while True:
+        frame = q.get()
+        frame = pickle.loads(frame)
+        msg, vol, type = frame
+        if type == "voiceMsg":
+            voice = set_volume(frame, vol)
+            hear.write(voice)
+
+
+
+
+t1 = threading.Thread(target=audio_stream_UDP, args=())
+t1.start()
+
+
---
+++ jsonic way/UDP_server.py
@@ -0,0 +1,169 @@
+# This is server code to send video and audio frames over UDP
+
+import socket, threading, time, pickle
+
+host_name = socket.gethostname()
+host_ip = '0.0.0.0'#  socket.gethostbyname(host_name)
+print(host_ip)
+UDP_port = 9633
+TCP_port = 9632
+clients_list = []
+addrs_list = []
+IDs_list = []
+##################################
+#receive from clients
+#Jotar
+#format example
+#PlayerInfo = {"PlayerId":PlayerId,"type":"voiceMessage", ,"ClientAddress","14.45.117.204", "TargetIds":[1,2,7],"Volume":[0.5,0.2,1.0] }
+#PlayersInfo ={"PlayerId",PlayerInfo}
+PlayersInfo = {}
+# For details visit: www.pyshine.com
+
+
+def check_connectivity():
+    #! Initializing TCP server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((host_ip, TCP_port))
+    s.listen()
+    print('TCP server listening at',(host_ip, (TCP_port)))
+
+    def accepting_connections():
+        while True:
+            conn, addr = s.accept()
+            ID = conn.recv(1024)
+            ID = ID.decode()
+            temp_arr = []
+            for arr in clients_list:
+                temp_arr.append(arr[1])
+            if ID not in temp_arr:
+                clients_list.append((conn,ID))
+
+    def checking_connections():
+        while True:
+            time.sleep(5)
+            for client in clients_list:
+                conn = client[0]
+                ID = client[1]
+                try:
+                    conn.send(b"boo!")
+                except ConnectionResetError:
+                    conn.close()
+                    clients_list.remove(client)   
+                    if ID in IDs_list:
+                        index =  IDs_list.index(ID)
+                        IDs_list.pop(index)
+                        addrs_list.pop(index)
+
+    t1 = threading.Thread(target=accepting_connections, args=())
+    t1.start()
+
+    t2 = threading.Thread(target=checking_connections, args=())
+    t2.start()
+
+
+
+def ReceivePlayersInfo():
+    BUFF_SIZE = 65536
+    ws = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    ws.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+    ws.bind((host_ip, (UDP_port)))
+    print('UDP server listening at client infos :: ',(host_ip, (UDP_port)))
+    while True:
+        PlayerInfo = {}
+        data = None
+        try:
+            data, addr = ws.recvfrom(BUFF_SIZE)
+            #TODO: convert bytes to json
+            PlayersInfo[PlayerInfo.PlayerId]=PlayerInfo
+        except ConnectionResetError:
+            pass
+
+def audio_stream_UDP():
+    BUFF_SIZE = 65536
+    #! Initializing UDP server
+    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+    s.bind((host_ip, (UDP_port)))
+    print('UDP server listening at',(host_ip, (UDP_port)))
+
+
+    # def conn_check():
+    #     while True:
+    #         time.sleep(5)
+    #         for client in IDs_list:
+    #             s.sendto("Still Connected?".encode() ,client)
+    #             msg,_ = s.recvfrom(BUFF_SIZE)
+
+    #             t_end = time.time() + 60 * 15
+    #             while time.time() < t_end:
+    #                 if msg == b"Still Connected":
+    #                     break
+    #             IDs_list.remove(client)
+
+
+    while True:
+        data = None
+        try:
+            data, addr = s.recvfrom(BUFF_SIZE)
+        except ConnectionResetError:
+            pass
+
+        if addr not in addrs_list:
+            playerID = data.decode()
+            if playerID not in IDs_list:
+                print(f'[GOT connection from]... {addr}, Name is: {playerID}')
+                s.sendto(f"Server Message: You Have been Connected: {playerID}".encode(),addr)   
+                addrs_list.append(addr)
+                IDs_list.append(playerID)
+            elif playerID in IDs_list:
+                index = IDs_list.index(playerID)
+                IDs_list.pop(index)
+                addrs_list.pop(index)
+                print(f'[GOT connection from]... {addr} Name is: {playerID}')
+                s.sendto(f"Server Message: You Have been Connected: {playerID}".encode(),addr)   
+                addrs_list.append(addr)
+                IDs_list.append(playerID)
+            print(IDs_list,"   ",addrs_list)
+            print(clients_list)
+            continue
+
+        #TODO: define SendTo to sent voice to
+        #first find the player id  of this audio sender
+
+        data = pickle.loads(data)
+        playerInfo, msg = data
+        type = playerInfo.type
+        if type == "voiceMsg":
+            targetIDs = playerInfo.Target_IDs
+            for recepient in targetIDs:
+                ID, vol = recepient
+                if ID in IDs_list:
+                    index = IDs_list.index(ID)
+                    destination = addrs_list[index]
+                    frame = [msg, vol, type]
+                    frame = pickle.dumps(frame)
+                    s.sendto(frame, destination)
+            time.sleep(0.1)
+
+            
+        # for Info in PlayersInfo:
+        #     if Info.ClientAddress == addr:
+        #         PlayerInfo=Info
+        # IdsSendTo=[]
+        # AddrSendTo=[]
+        # IdsSendTo=PlayerInfo.TargetIds
+        # if    PlayerInfo !=None:     
+        #         for Info in PlayersInfo:
+        #             if  Info.PlayerId in IdsSendTo:
+        #                 AddrSendTo.append(Info.ClientAddress)
+
+
+                    
+
+
+t1 = threading.Thread(target=audio_stream_UDP, args=())
+t1.start()
+
+t2 = threading.Thread(target=check_connectivity, args=())
+t2.start()
